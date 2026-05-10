2f7dc4d4caf49720fc91f4884add8dc9a5c8f882
jea022 <aguinagajuanes2003@gmail.com>
2026-05-10 04:12:22 +0200

    Fix image paths for GitHub Pages (remove leading slash)

--- App.tsx
+++ App.tsx
@@ -217,7 +217,7 @@ const App: React.FC = () => {
             <BentoCard className="p-8 flex flex-col justify-between group overflow-hidden" onClick={() => setActiveModal('projects')}>
               <div className="flex justify-center py-4 h-32 relative">
                 <div className="w-full h-full rounded-xl overflow-hidden">
-                  <img src="/projects-cover.jpg" alt="Projects Preview" className="w-full h-full object-cover opacity-80 group-hover:opacity-100 group-hover:scale-105 transition-all duration-500" />
+                  <img src="projects-cover.jpg" alt="Projects Preview" className="w-full h-full object-cover opacity-80 group-hover:opacity-100 group-hover:scale-105 transition-all duration-500" />
                 </div>
               </div>
               <div>
